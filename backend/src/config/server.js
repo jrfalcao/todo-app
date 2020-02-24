02197f59fb433c649c37de077f21b5021fbb30bd
jrfalcao <juniorfalcao.jc@gmail.com>
2020-02-23 21:52:12 -0300

    compartilhando recursos da Api

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,12 +1,18 @@
 const port = 3003
 
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const express = require('express')
+
 const server = express()
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 
+server.use(cors())
+
 server.listen(port, () => {
-    console.log(`BACKEND is running on port ${port}`)
+    console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
